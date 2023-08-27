496dc4c66941693f9a24ac1a0f2ce2e8e80461b1
Nicky Ask <loevendallund@gmail.com>
2023-08-27 15:38:45 +0200

    Added communication between gpsmodule manager and flask server through redis

--- gps.py
+++ gps.py
@@ -3,6 +3,7 @@ from pyubx2 import UBXReader, UBXMessage
 from threading import Thread
 import time
 from gpiozero import LED
+import redis
 
 class GPSModule(Thread):
     gps = None
@@ -11,35 +12,42 @@ class GPSModule(Thread):
     lng = None
     active = True
     led = LED(3)
+    redis = None
 
-    def __init__(self, portname="/dev/ttyACM0", timeout=2, baudrate=9600):
+    def __init__(self, redis_inst, portname="/dev/ttyACM0", timeout=2, baudrate=9600):
+        self.redis = redis_inst
         self.gps = Serial(portname, timeout=timeout, baudrate=baudrate)
         self.streamReader = UBXReader(self.gps)
+        self.led.blink(on_time=1, off_time=1)
         Thread.__init__(self)
         self.start()
 
     def run(self):
         if self.streamReader == None:
             return
-        self.led.blink(on_time=1, off_time=1)
         while self.active:
             self.readData()
             if self.lat != None and self.lng != None:
                 self.led.on()
                 print("Latitude: " + str(self.lat) + ", Longitude: " + str(self.lng))
                 time.sleep(1.0)
-            
+
     def close(self):
         self.active = false
         self.gps.close()
 
     def readData(self):
         (_, data) = self.streamReader.read()
+        if data == None or type(data) == str:
+            return
+        print(data)
         if data.identity == "GPRMC":
             if type(data.__dict__["lat"]) == float:
                 self.lat = data.__dict__["lat"]
+                self.redis.set("lat", self.lat)
             if type(data.__dict__["lon"]) == float:
                 self.lng = data.__dict__["lon"]
+                self.redis.set("lng", self.lng)
 
-module = GPSModule()
-#module.Signal()
+if __name__ == '__main__':
+    GPSModule(redis_inst=redis.Redis("localhost"))
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, g
+from gps import GPSModule
+from flask_redis import FlaskRedis
+import redis
 
 app = Flask(__name__)
+#redis = FlaskRedis(app)
+
+r = redis.Redis(host='localhost', decode_responses=True)
 
 @app.route('/')
 def index():
@@ -8,8 +14,15 @@ def index():
 
 @app.route('/api/coordinates', methods = ['GET'])
 def getCoords():
-    return jsonify({'lat': 57.054, 'lon': 9.912})
+    lat = None
+    lng = None
+    if r.exists("lat"):
+        lat = r.get("lat")
+    if r.exists("lng"):
+        lng = r.get("lng")
 
+    return jsonify({"lat": lat, "lng": lng})
 
 if __name__ == '__main__':
-    app.run()
+    gps = GPSModule(redis_inst=r)
+    app.run(debug=True, host="0.0.0.0")
